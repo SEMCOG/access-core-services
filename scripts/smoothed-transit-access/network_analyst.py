ec381bc0e413028569426fa123c3170cd6e7ee9a
Jimmy McBroom <james.mcbroom@gmail.com>
2015-06-16 16:15:24 -0400

    cut down results file size

--- scripts/smoothed-transit-access/network_analyst.py
+++ scripts/smoothed-transit-access/network_analyst.py
@@ -11,9 +11,9 @@ start_times = ['8:{0}'.format(str(x).zfill(2)) for x in xrange(30)]
 
 for time in start_times:
     eval_time = "6/1/2015 {0}:00 AM".format(time)
-    
+
     print "Making OD Cost Matrix Layer for {0}".format(eval_time)
-    
+
     outNALayer = arcpy.MakeODCostMatrixLayer_na(in_network_dataset="C:/Users/mcbroom/Desktop/access.gdb/semTransit/semTransit_ND",
                                     out_network_analysis_layer="OD Cost Matrix",
                                     impedance_attribute="Transit_TT",
@@ -26,19 +26,19 @@ for time in start_times:
                                     hierarchy_settings="#",
                                     output_path_shape="NO_LINES",
                                     time_of_day=eval_time)
-    
+
     print "Made OD Cost Matrix Layer"
-    
+
     outNALayer = outNALayer.getOutput(0)
-    
+
     subLayerNames = arcpy.na.GetNAClassNames(outNALayer)
-    
+
     originsLayerName = subLayerNames["Origins"]
     destinationsLayerName = subLayerNames["Destinations"]
     linesLayerName = subLayerNames["ODLines"]
-    
+
     print "Adding Destinations"
-    
+
     # add grocery stores
     arcpy.AddLocations_na(in_network_analysis_layer="OD Cost Matrix",
                             sub_layer="Destinations",
@@ -53,11 +53,11 @@ for time in start_times:
                             snap_offset="5 Meters",
                             exclude_restricted_elements="INCLUDE",
                             search_query="Connectors_Stops2Streets #;Streets_UseThisOne #;TransitLines #;Stops #;Stops_Snapped2Streets #;semTransit_ND_Junctions #")
-    
+
     # add origins
     print "Adding origins"
     arcpy.AddLocations_na(in_network_analysis_layer="OD Cost Matrix",sub_layer="Origins",in_table="origin_naTransitCentroids",field_mappings="Name nodeid #",search_tolerance="5000 Meters",sort_field="#",search_criteria="Connectors_Stops2Streets NONE;Streets_UseThisOne SHAPE;TransitLines NONE;Stops NONE;Stops_Snapped2Streets NONE;semTransit_ND_Junctions NONE",match_type="MATCH_TO_CLOSEST",append="APPEND",snap_to_position_along_network="NO_SNAP",snap_offset="5 Meters",exclude_restricted_elements="INCLUDE",search_query="Connectors_Stops2Streets #;Streets_UseThisOne #;TransitLines #;Stops #;Stops_Snapped2Streets #;semTransit_ND_Junctions #")
-    
+
     print "Begin Solving"
     arcpy.na.Solve(outNALayer)
     print "Done Solving"
@@ -70,9 +70,8 @@ for time in start_times:
                 with arcpy.da.SearchCursor(lyr, fields) as cursor:
                     print "Successfully created lines searchCursor.  Exporting to " + outFile
                     for row in cursor:
-                        f.write(','.join([str(r) for r in row])+'\n')
-                        
-    arcpy.Delete_management(outNALayer)
-    
-arcpy.CheckInExtension("Network")
+                        f.write(','.join([str(r).replace(',',' ') for r in row])+'\n')
 
+    arcpy.Delete_management(outNALayer)
+
+arcpy.CheckInExtension("Network")
--- scripts/smoothed-transit-access/process_results.py
+++ scripts/smoothed-transit-access/process_results.py
@@ -2,27 +2,27 @@ import pandas as pd, os, sys
 
 # run this in the same folder as the results files generated with network_analyst.py
 results = [x for x in os.listdir(os.getcwd()) if x.endswith(".csv")]
-destination = results.split('_')[0]
- 
+destination = results[0].split('_')[0]
+
 minTT_df = pd.DataFrame()
 total_df = pd.DataFrame()
- 
+
 for result in results:
     time = result.split('_')[-1].split('.')[0]
     df = pd.read_csv(result)
-    
+
     # only need this if applying new cut-off
     # df = df[df['Total_Transit_TT'] <= 30]
-    
+
     df['ori'],df['dest'] = zip(*df['Name'].apply(lambda x: x.split(' - ',1)))
     df.drop('Name', inplace=True, axis=1)
     total_series = df['ori'].value_counts()
     minTT_series = df.groupby(['ori'], sort=False)['Total_Transit_TT'].min()
     minTT_df['result_{0}'.format(time)] = minTT_series
     total_df['result_{0}'.format(time)] = total_series
-    
+
 total_df['hi_access'] = total_df[total_df.columns].max(axis=1)
 minTT_df['lo_transittime'] = minTT_df[minTT_df.columns].min(axis=1)
- 
+
 total_df.to_csv('{0}_numberReachable.csv'.format(destination))
 minTT_df.to_csv('{0}_minimumTravelTime.csv'.format(destination))
